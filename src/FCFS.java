392ad09bd752a622fa2673ac598d9bb6318316c2
Raymel Francisco <franciscoraymel@gmail.com>
2016-11-27 20:52:49 +0800

    NEW algorithm added: First Come-First Serve

---
+++ src/FCFS.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
+
+public class FCFS extends DischedAlgoBase {
+
+	public FCFS(int previous, int current, int seekRate, ArrayList<Integer> tracks) {
+		
+		this.points = tracks;
+
+		this.previous = previous;
+		this.current = current;
+		this.seekRate = seekRate;
+	}
+
+	@Override // from DischedAlgoBase
+	public ArrayList<Integer> getPoints() {
+		return points;
+	}
+}
